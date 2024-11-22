66d3d78ec79a90fd9726c2075e3c794465f2f314
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2024-11-21 23:51:23 -0500

    Update a.java

--- a.java
+++ a.java
@@ -1,57 +1,18 @@
 package com.ms.datalink.globalDatalink.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class UploadResourceResponse {
     private String processId;
     private int submissionId;
     private List<DocumentIdEntry> documentIds;
 
-    // Getters and Setters
-    public String getProcessId() {
-        return processId;
-    }
-
-    public void setProcessId(String processId) {
-        this.processId = processId;
-    }
-
-    public int getSubmissionId() {
-        return submissionId;
-    }
-
-    public void setSubmissionId(int submissionId) {
-        this.submissionId = submissionId;
-    }
-
-    public List<DocumentIdEntry> getDocumentIds() {
-        return documentIds;
-    }
-
-    public void setDocumentIds(List<DocumentIdEntry> documentIds) {
-        this.documentIds = documentIds;
-    }
-
-    // Inner class to represent each document ID entry
+    @Data
     public static class DocumentIdEntry {
         private String name;
         private int documentId;
-
-        // Getters and Setters
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getDocumentId() {
-            return documentId;
-        }
-
-        public void setDocumentId(int documentId) {
-            this.documentId = documentId;
-        }
     }
 }
