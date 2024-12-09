f3b944d227f155141a97f558b84cd11118c3dcd5
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2024-12-09 13:58:00 -0500

    Update a.java

--- a.java
+++ a.java
@@ -1,9 +1,2 @@
-Subject: Request for Approval of Release Jira
-
-Hi Hilary,
-
-I hope this message finds you well.
-
-I am part of the MSIM Datafeed team, and we are planning to proceed with a production release. To move forward, we kindly request your assistance in approving the associated release Jira.
-
-Your approval would enable us to proceed as planned.
+Infra Downtime: No.
+Reason: The production release involves promoting changes to the production environment, followed by a restart of the project. This process does not impact the availability of the system, ensuring there is no downtime.
