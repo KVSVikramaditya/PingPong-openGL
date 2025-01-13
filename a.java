f85e2242e1ca182277ff97ecc9040ac9263d4b0f
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-01-12 22:06:28 -0500

    Update a.java

--- a.java
+++ a.java
@@ -1,42 +1,38 @@
- public static void uploadFile(String filePath, String authToken) throws IOException {
-        // Create the OkHttpClient instance
+import okhttp3.*;
+
+public class MultipartRequestWithoutFile {
+
+    public static void main(String[] args) throws Exception {
         OkHttpClient client = new OkHttpClient();
 
-        // Define the file and read its bytes
-        File file = new File(filePath);
-        if (!file.exists()) {
-            throw new IOException("File not found: " + filePath);
-        }
-        byte[] fileBytes = Files.readAllBytes(file.toPath());
-
-        // Define the boundary
+        // Define boundary
         String boundary = "011000010111000001101001";
 
-        // Create the multipart request body
-        RequestBody body = new MultipartBody.Builder(boundary)
-                .setType(MultipartBody.FORM)
-                .addFormDataPart(
-                        "content", 
-                        file.getName(),
-                        RequestBody.create(MediaType.parse("application/octet-stream"), fileBytes)
-                )
-                .build();
+        // Construct the multipart body manually
+        String multipartBody = "--" + boundary + "\r\n" +
+                "Content-Disposition: form-data; name=\"content\"\r\n\r\n" +
+                "fileByteArrayContentHere\r\n" +
+                "--" + boundary + "--";
 
-        // Build the HTTP request
+        // Create the RequestBody
+        MediaType mediaType = MediaType.parse("multipart/form-data; boundary=" + boundary);
+        RequestBody body = RequestBody.create(mediaType, multipartBody);
+
+        // Build the request
         Request request = new Request.Builder()
                 .url("https://api.seismic.com/integration/v2/teamsites/03793088-6c8c-40f5-9cb4-e930491dab43/files/b5b7727e-18d5-4b48-9f78-62b7111468cb/content")
                 .put(body)
-                .addHeader("Authorization", "Bearer " + authToken)
                 .addHeader("accept", "application/json; charset=utf-8")
                 .addHeader("content-type", "multipart/form-data; boundary=" + boundary)
                 .build();
 
-        // Execute the request and get the response
+        // Execute the request
         try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new IOException("Unexpected response: " + response);
+            if (response.isSuccessful()) {
+                System.out.println("Response: " + response.body().string());
+            } else {
+                System.err.println("Failed with HTTP code: " + response.code());
             }
-            System.out.println("Response: " + response.body().string());
         }
     }
 }
