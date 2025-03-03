e55aa6788858ee3f54bce5b638f4429165702d40
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-03-03 15:24:55 -0500

    Update a.java

--- a.java
+++ a.java
@@ -1,10 +1,21 @@
 package com.msim.seismic_datafeed.jobs.salescoverage;
 
-import com.msim.seismic_datafeed.utils.CSVFileUtils;
-import com.msim.seismic_datafeed.utils.FileUtils;
+import com.msim.seismic_datafeed.constants.JobType;
+import com.msim.seismic_datafeed.jobs.CometJobTrigger;
+import com.msim.seismic_datafeed.jobs.StatisticsLoggerJobExecutionListener;
+import com.msim.seismic_datafeed.jobs.salescoverage.model.SalescoverageData;
+import com.msim.seismic_datafeed.jobs.salescoverage.model.SalescoverageDataItem;
+import com.msim.seismic_datafeed.model.FileItem;
+import com.msim.seismic_datafeed.utils.FileItemProcessor;
+import com.msim.seismic_datafeed.utils.ListUnpackingItemWriter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -13,17 +24,17 @@ import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
-@Slf4j
 @Configuration
+@EnableBatchProcessing
+@Slf4j
 public class SalescoverageBatchConfiguration {
 
     @Autowired
@@ -33,34 +44,31 @@ public class SalescoverageBatchConfiguration {
     private StepBuilderFactory stepBuilderFactory;
 
     @Autowired
-    private JobExecutionDecider cometJobDecider;
-
-    @Autowired
     private CometJobTrigger cometJobTrigger;
 
     @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private JobExecutionDecider cometJobDecider;
 
     @Value("${spring.batch.chunk.size}")
     private int chunkSize;
 
-    @Value("${seismic.output.file.directory}")
+    @Value("${seismic.output.file.directory:}")
     private String outputFileDirectory;
 
-    @Bean(name = "LoadSalescoverageJob")
+    @Bean(name = "loadSalescoverageJob")
     public Job loadSalescoverageJob(
             @Qualifier("retrieveSalescoverage") Step retrieveSalescoverage,
             @Qualifier("salescoverageCometJobTriggerStep") Step salescoverageCometJobTriggerStep,
             StatisticsLoggerJobExecutionListener statisticsLoggerJobExecutionListener) {
-        return jobBuilderFactory.get("LoadSalescoverageJob")
+        return jobBuilderFactory.get("loadSalescoverageJob")
                 .listener(statisticsLoggerJobExecutionListener)
                 .incrementer(new RunIdIncrementer())
                 .preventRestart()
                 .flow(retrieveSalescoverage)
                 .next(cometJobDecider)
-                .on(CometJobDecider.CONTINUE).to(salescoverageCometJobTriggerStep)
+                .on("CONTINUE").to(salescoverageCometJobTriggerStep)
                 .from(cometJobDecider)
-                .on(CometJobDecider.BREAK).end()
+                .on("BREAK").end()
                 .end()
                 .build();
     }
@@ -68,57 +76,69 @@ public class SalescoverageBatchConfiguration {
     @Bean(name = "retrieveSalescoverage")
     public Step retrieveSalescoverage(
             @Qualifier("salescoverageDataReader") SalescoverageDataReader salescoverageDataReader,
-            @Qualifier("salescoverageItemProcessor") CompositeItemProcessor<SalescoverageInput, SalescoverageOutput> salescoverageItemProcessor,
-            @Qualifier("salescoverageFileItemWriter") FlatFileItemWriter<SalescoverageOutput> salescoverageFileItemWriter) {
+            @Qualifier("salescoverageItemProcessor") CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> salescoverageItemProcessor,
+            @Qualifier("salescoverageFlatFileItemWriter") FlatFileItemWriter<SalescoverageDataItem> fileItemWriter,
+            @Value("${spring.batch.chunk.size}") int chunkSize) {
         return stepBuilderFactory.get("retrieveSalescoverage")
-                .<SalescoverageInput, SalescoverageOutput>chunk(chunkSize)
+                .<SalescoverageData, SalescoverageDataItem>chunk(chunkSize)
                 .reader(salescoverageDataReader)
                 .processor(salescoverageItemProcessor)
-                .writer(salescoverageFileItemWriter)
+                .writer(fileItemWriter)
                 .build();
     }
 
     @Bean(name = "salescoverageDataReader")
     @StepScope
-    public SalescoverageDataReader salescoverageDataReader(SalescoverageDataProvider salescoverageDataProvider) {
-        return new SalescoverageDataReader(salescoverageDataProvider);
+    public SalescoverageDataReader salescoverageDataReader() {
+        return new SalescoverageDataReader();
     }
 
     @Bean(name = "salescoverageDataProvider")
-    public SalescoverageDataProvider salescoverageDataProvider() {
+    public SalescoverageDataProvider salescoverageDataProvider(@Qualifier("snowflakeJdbcTemplate") NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         return new SalescoverageDataProvider(namedParameterJdbcTemplate);
     }
 
     @Bean(name = "salescoverageItemProcessor")
     @StepScope
-    public CompositeItemProcessor<SalescoverageInput, SalescoverageOutput> salescoverageItemProcessor(
-            @Autowired SalescoverageDataProcessor salescoverageDataProcessor) throws Exception {
-        List<Object> delegates = new ArrayList<>(1);
-        delegates.add(salescoverageDataProcessor);
-        CompositeItemProcessor<SalescoverageInput, SalescoverageOutput> compositeItemProcessor = new CompositeItemProcessor<>();
-        compositeItemProcessor.setDelegates(delegates);
-        compositeItemProcessor.afterPropertiesSet();
-        return compositeItemProcessor;
+    public CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> salescoverageItemProcessor(
+            @Qualifier("salescoverageProcessor") SalescoverageDataProcessor salescoverageDataProcessor,
+            @Autowired FileItemProcessor fileItemProcessor) throws Exception {
+        CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> compositeProcessor = new CompositeItemProcessor<>();
+        compositeProcessor.setDelegates(java.util.List.of(salescoverageDataProcessor, fileItemProcessor));
+        compositeProcessor.afterPropertiesSet();
+        return compositeProcessor;
     }
 
-    @Bean(name = "salescoverageFileItemWriter")
+    @Bean(name = "salescoverageProcessor")
     @StepScope
-    public FlatFileItemWriter<SalescoverageOutput> salescoverageFileItemWriter(
-            @Value("#{jobParameters['asOfDate']}") String asOfDate) {
-        String fileName = "SalesCoverage_" + asOfDate + ".csv";
-        String fullFilePath = outputFileDirectory + fileName;
-        FileSystemResource fileResource = new FileSystemResource(fullFilePath);
-        BeanWrapperFieldExtractor<SalescoverageOutput> fieldExtractor = new BeanWrapperFieldExtractor<>();
-        fieldExtractor.setNames(new String[]{"field1", "field2", "field3"}); // Modify as needed
-        DelimitedLineAggregator<SalescoverageOutput> lineAggregator = new DelimitedLineAggregator<>();
-        lineAggregator.setDelimiter(",");
-        lineAggregator.setFieldExtractor(fieldExtractor);
-        FlatFileItemWriter<SalescoverageOutput> fileItemWriter = new FlatFileItemWriter<>();
-        fileItemWriter.setResource(fileResource);
-        fileItemWriter.setLineAggregator(lineAggregator);
-        fileItemWriter.setShouldDeleteIfEmpty(true);
-        fileItemWriter.setShouldDeleteIfExists(true);
-        return fileItemWriter;
+    public SalescoverageDataProcessor salescoverageProcessor() {
+        return new SalescoverageDataProcessor();
     }
 
+    @Bean(name = "salescoverageFlatFileItemWriter")
+    @StepScope
+    public FlatFileItemWriter<SalescoverageDataItem> salescoverageFlatFileItemWriter(
+            @Qualifier("jobScopedStore") JobScopedStore store,
+            @Qualifier("salescoverageFlatFileHeaderCallback") SalescoverageFlatFileHeaderCallback headerCallback,
+            @Value("#{jobParameters['startTime']}") long startTime,
+            @Value("#{jobParameters['asOfDate']}") String asOfDate) {
+        String fileName = JobType.SALESCOVERAGE.name() + "_" + asOfDate + "_" + startTime + ".csv";
+        String fullFilePath = outputFileDirectory + fileName;
+        store.setOutputFileName(fileName);
+        
+        BeanWrapperFieldExtractor<SalescoverageDataItem> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"sfTeamId", "teamCode", "teamName", "sfTerritoryId", "territoryCode", "territoryName", "internalSalesPersonMsid", "internalSalesPersonFullName", "externalSalesPersonMsid", "externalSalesPersonFullName"});
+        
+        DelimitedLineAggregator<SalescoverageDataItem> aggregator = new DelimitedLineAggregator<>();
+        aggregator.setDelimiter(",");
+        aggregator.setFieldExtractor(fieldExtractor);
+        
+        FlatFileItemWriter<SalescoverageDataItem> writer = new FlatFileItemWriter<>();
+        writer.setResource(new FileSystemResource(fullFilePath));
+        writer.setHeaderCallback(headerCallback);
+        writer.setLineAggregator(aggregator);
+        writer.setShouldDeleteIfEmpty(true);
+        writer.setShouldDeleteIfExists(true);
+        return writer;
+    }
 }
