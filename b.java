ed8f7348511dc0002c6fa412d7d8d8ca5cde4011
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-03-03 19:07:22 -0500

    Update b.java

--- b.java
+++ b.java
@@ -10,3 +10,33 @@ public CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> salescov
     
     return compositeProcessor;
 }
+
+
+@Bean(name = "retrieveSalescoverage")
+public Step retrieveSalescoverage(
+        @Qualifier("salescoverageDataReader") SalescoverageDataReader salescoverageDataReader,
+        @Qualifier("salescoverageItemProcessor") CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> salescoverageItemProcessor,
+        @Qualifier("salescoverageFlatFileItemWriter") FlatFileItemWriter<SalescoverageDataItem> fileItemWriter,
+        @Value("${spring.batch.chunk.size}") int chunkSize
+) {
+    return stepBuilderFactory.get("retrieveSalescoverage")
+            .<SalescoverageData, SalescoverageDataItem>chunk(chunkSize)
+            .reader(salescoverageDataReader)
+            .processor(salescoverageItemProcessor)
+            .writer(fileItemWriter)
+            .build();
+}
+
+
+@Override
+public SalescoverageData read() throws Exception {
+    if (data == null) {
+        data = salescoverageDataProvider.fetchSalescoverageData();
+    }
+    if (nextIndex < data.size()) {
+        return data.get(nextIndex++);
+    } else {
+        return null; // Signals the end of reading
+    }
+}
+
