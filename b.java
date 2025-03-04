dc03918e4f7d67ee025bcdd1b5ccac5aa3c6be0a
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-03-03 19:06:01 -0500

    Update b.java

--- b.java
+++ b.java
@@ -1,8 +1,12 @@
-@Bean(name = "salescoverageCometJobTriggerStep")
-public Step salescoverageCometJobTriggerStep(@Qualifier("jobScopedStore") JobScopedStore store) {
-    Tasklet tasklet = (StepContribution contribution, ChunkContext chunkContext) -> {
-        cometJobTrigger.submit(store.getOutputFileName());
-        return RepeatStatus.FINISHED;
-    };
-    return stepBuilderFactory.get("salescoverageCometJobTriggerStep").tasklet(tasklet).build();
+@Bean(name = "salescoverageItemProcessor")
+@StepScope
+public CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> salescoverageItemProcessor(
+        @Qualifier("salescoverageProcessor") SalescoverageDataProcessor salescoverageDataProcessor,
+        @Autowired FileItemProcessor fileItemProcessor) throws Exception {
+    
+    CompositeItemProcessor<SalescoverageData, SalescoverageDataItem> compositeProcessor = new CompositeItemProcessor<>();
+    compositeProcessor.setDelegates(java.util.List.of(salescoverageDataProcessor, fileItemProcessor));
+    compositeProcessor.afterPropertiesSet();
+    
+    return compositeProcessor;
 }
