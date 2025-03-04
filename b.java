5e7cdcd925d773d5a0dde832cbf7ecfe724fb31d
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-03-03 19:09:43 -0500

    Update b.java

--- b.java
+++ b.java
@@ -28,15 +28,45 @@ public Step retrieveSalescoverage(
 }
 
 
-@Override
-public SalescoverageData read() throws Exception {
-    if (data == null) {
-        data = salescoverageDataProvider.fetchSalescoverageData();
-    }
-    if (nextIndex < data.size()) {
-        return data.get(nextIndex++);
-    } else {
-        return null; // Signals the end of reading
+package com.msim.seismic_datafeed.jobs.salescoverage;
+
+import com.msim.seismic_datafeed.jobs.salescoverage.model.SalescoverageData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class SalescoverageDataReader implements ItemReader<SalescoverageData> {
+
+    @Autowired
+    private SalescoverageDataProvider salescoverageDataProvider;
+
+    private List<SalescoverageData> data;
+    private int nextIndex = 0;
+
+    @Override
+    public SalescoverageData read() throws Exception {
+        // Fetch data only once
+        if (data == null) {
+            log.info("Fetching sales coverage data from provider...");
+            data = salescoverageDataProvider.fetchSalescoverageData();
+            nextIndex = 0;
+        }
+
+        // Read next record
+        if (nextIndex < data.size()) {
+            SalescoverageData record = data.get(nextIndex++);
+            log.info("Reading record: {}", record);
+            return record;
+        } else {
+            log.info("No more data available. Returning null to signal end of reading.");
+            return null;  // Signals the end of reading to Spring Batch
+        }
     }
 }
+
 
