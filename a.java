492e85f0f9b297d06e091b6e7ff91b26137f4c55
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-03-12 00:47:54 -0400

    Update a.java

--- a.java
+++ a.java
@@ -1,82 +1,49 @@
-package com.msim.seismic_datafeed.dao;
-
-import com.msim.seismic_datafeed.jobs.salescoverage.model.SalescoverageData;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@Slf4j
-@Component
-public class SalescoverageDaoImpl {
-
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    public SalescoverageDaoImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    }
-
-    public List<SalescoverageData> getSalesCoverageRecords() {
-        log.info("Executing query to fetch sales coverage data from Snowflake");
-
-        String sql = "SELECT SF_TEAM_ID, TEAM_CODE, TEAM_NAME, SF_TERRITORY_ID, TERRITORY_CODE, TERRITORY_NAME," +
-                     " INTERNAL_SALES_PERSON_MSID, INTERNAL_SALES_PERSON_FULL_NAME," +
-                     " EXTERNAL_SALES_PERSON_MSID, EXTERNAL_SALES_PERSON_FULL_NAME " +
-                     "FROM sales_coverage_table";
-
-        List<Map<String, Object>> rows = namedParameterJdbcTemplate.queryForList(sql, new MapSqlParameterSource());
-        return rows.stream().map(row -> {
-            SalescoverageData data = new SalescoverageData();
-            data.setSfTeamId((String) row.get("SF_TEAM_ID"));
-            data.setTeamCode((String) row.get("TEAM_CODE"));
-            data.setTeamName((String) row.get("TEAM_NAME"));
-            data.setSfTerritoryId((String) row.get("SF_TERRITORY_ID"));
-            data.setTerritoryCode((String) row.get("TERRITORY_CODE"));
-            data.setTerritoryName((String) row.get("TERRITORY_NAME"));
-            data.setInternalSalesPersonMsid((String) row.get("INTERNAL_SALES_PERSON_MSID"));
-            data.setInternalSalesPersonFullName((String) row.get("INTERNAL_SALES_PERSON_FULL_NAME"));
-            data.setExternalSalesPersonMsid((String) row.get("EXTERNAL_SALES_PERSON_MSID"));
-            data.setExternalSalesPersonFullName((String) row.get("EXTERNAL_SALES_PERSON_FULL_NAME"));
-            return data;
-        }).collect(Collectors.toList());
-    }
-}
-
-
-
 package com.msim.seismic_datafeed.jobs.salescoverage;
 
-import com.msim.seismic_datafeed.dao.SalescoverageDaoImpl;
 import com.msim.seismic_datafeed.jobs.salescoverage.model.SalescoverageData;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Service;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import java.util.List;
+import static org.testng.Assert.assertEquals;
 
-@Slf4j
-@Service
-public class SalescoverageDataProvider {
+public class SalescoverageDataTest {
 
-    private final SalescoverageDaoImpl dao;
+    private SalescoverageData salescoverageData;
 
-    @Autowired
-    public SalescoverageDataProvider(NamedParameterJdbcTemplate jdbcTemplate) {
-        // Internally create the DAO
-        this.dao = new SalescoverageDaoImpl(jdbcTemplate);
+    @BeforeMethod
+    public void setUp() {
+        salescoverageData = new SalescoverageData();
+
+        salescoverageData.setSTeamId("team123");
+        salescoverageData.setTeamCode("TC001");
+        salescoverageData.setTeamName("Risk Analysis Team");
+        salescoverageData.setSfTerritoryId("territory456");
+        salescoverageData.setTerritoryCode("TER123");
+        salescoverageData.setTerritoryName("North America");
+        salescoverageData.setInternalSalesPersonMsid("MSID789");
+        salescoverageData.setInternalSalesPersonFullName("John Doe");
+        salescoverageData.setExternalSalesPersonMsid("MSID456");
+        salescoverageData.setExternalSalesPersonFullName("Jane Smith");
     }
 
-    /**
-     * Fetches multiple SalescoverageData records from Snowflake.
-     */
-    public List<SalescoverageData> fetchSalescoverageData() {
-        log.info("Fetching sales coverage data from DAO (internally created).");
-        return dao.getSalesCoverageRecords();
+    @Test
+    public void testSalescoverageDataFields() {
+        assertEquals(salescoverageData.getSTeamId(), "team123");
+        assertEquals(salescoverageData.getTeamCode(), "TC001");
+        assertEquals(salescoverageData.getTeamName(), "Risk Analysis Team");
+        assertEquals(salescoverageData.getSfTerritoryId(), "territory456");
+        assertEquals(salescoverageData.getTerritoryCode(), "TER123");
+        assertEquals(salescoverageData.getTerritoryName(), "North America");
+        assertEquals(salescoverageData.getInternalSalesPersonMsid(), "MSID789");
+        assertEquals(salescoverageData.getInternalSalesPersonFullName(), "John Doe");
+        assertEquals(salescoverageData.getExternalSalesPersonMsid(), "MSID456");
+        assertEquals(salescoverageData.getExternalSalesPersonFullName(), "Jane Smith");
+    }
+
+    @Test
+    public void testSalescoverageDataToString() {
+        String expectedString = "SalescoverageData(sTeamId=team123, teamCode=TC001, teamName=Risk Analysis Team, sfTerritoryId=territory456, territoryCode=TER123, territoryName=North America, internalSalesPersonMsid=MSID789, internalSalesPersonFullName=John Doe, externalSalesPersonMsid=MSID456, externalSalesPersonFullName=Jane Smith)";
+
+        assertEquals(salescoverageData.toString(), expectedString);
     }
 }
-
