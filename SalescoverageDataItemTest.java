dfd01202903d3bea346fb39c1824662283e209fd
Vikramaditya <43729810+KVSVikramaditya@users.noreply.github.com>
2025-03-25 01:12:28 -0400

    Update SalescoverageDataItemTest.java

--- SalescoverageDataItemTest.java
+++ SalescoverageDataItemTest.java
@@ -1,49 +1,6 @@
-package com.msim.seismic_datafeed.jobs.salescoverage;
+dependencies {
+    implementation "org.springframework.boot:spring-boot-starter-security"
 
-import com.msim.seismic_datafeed.jobs.salescoverage.model.SalescoverageDataItem;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
-
-public class SalescoverageDataItemTest {
-
-    private SalescoverageDataItem salescoverageDataItem;
-
-    @BeforeMethod
-    public void setUp() {
-        salescoverageDataItem = new SalescoverageDataItem();
-
-        salescoverageDataItem.setSTeamId("team123");
-        salescoverageDataItem.setTeamCode("TC001");
-        salescoverageDataItem.setTeamName("Risk Analysis Team");
-        salescoverageDataItem.setSfTerritoryId("territory456");
-        salescoverageDataItem.setTerritoryCode("TER123");
-        salescoverageDataItem.setTerritoryName("North America");
-        salescoverageDataItem.setInternalSalesPersonMsid("MSID789");
-        salescoverageDataItem.setInternalSalesPersonFullName("John Doe");
-        salescoverageDataItem.setExternalSalesPersonMsid("MSID456");
-        salescoverageDataItem.setExternalSalesPersonFullName("Jane Smith");
-    }
-
-    @Test
-    public void testSalescoverageDataItemFields() {
-        assertEquals(salescoverageDataItem.getSTeamId(), "team123");
-        assertEquals(salescoverageDataItem.getTeamCode(), "TC001");
-        assertEquals(salescoverageDataItem.getTeamName(), "Risk Analysis Team");
-        assertEquals(salescoverageDataItem.getSfTerritoryId(), "territory456");
-        assertEquals(salescoverageDataItem.getTerritoryCode(), "TER123");
-        assertEquals(salescoverageDataItem.getTerritoryName(), "North America");
-        assertEquals(salescoverageDataItem.getInternalSalesPersonMsid(), "MSID789");
-        assertEquals(salescoverageDataItem.getInternalSalesPersonFullName(), "John Doe");
-        assertEquals(salescoverageDataItem.getExternalSalesPersonMsid(), "MSID456");
-        assertEquals(salescoverageDataItem.getExternalSalesPersonFullName(), "Jane Smith");
-    }
-
-    @Test
-    public void testSalescoverageDataItemToString() {
-        String expectedString = "SalescoverageDataItem(sTeamId=team123, teamCode=TC001, teamName=Risk Analysis Team, sfTerritoryId=territory456, territoryCode=TER123, territoryName=North America, internalSalesPersonMsid=MSID789, internalSalesPersonFullName=John Doe, externalSalesPersonMsid=MSID456, externalSalesPersonFullName=Jane Smith)";
-
-        assertEquals(salescoverageDataItem.toString(), expectedString);
-    }
+    implementation "org.springframework.security:spring-security-kerberos-web:1.0.1.RELEASE"
+    implementation "org.springframework.security:spring-security-kerberos-core:1.0.1.RELEASE"
 }
